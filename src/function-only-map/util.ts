1b8f8a7dcd9618e7b09fb0f24a7a23cfd6cbad7c
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-05 00:04:15 +0200

    improve typing

--- src/function-only-map/util.ts
+++ src/function-only-map/util.ts
@@ -1,7 +1,9 @@
+type Pair<t1, t2> = (first: boolean) => t1 | t2 | null;
+
 export const createPair =
-  <t1, t2>(fst: t1, snd: t2) =>
+  <t1, t2>(fst: t1, snd: t2): Pair<t1, t2> =>
   (first: boolean) =>
     first ? fst : snd;
 
-export const first = <t>(pair: (first: boolean) => t) => pair(true);
-export const second = <t>(pair: (first: boolean) => t) => pair(false);
+export const first = <t1, t2>(pair: Pair<t1, t2>) => pair(true);
+export const second = <t1, t2>(pair: Pair<t1, t2>) => pair(false);
