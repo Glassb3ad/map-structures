62c5c92117a29cf49ad56232d2c0f6c241bcd741
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-06 01:11:20 +0200

    moved linked list to its own file

--- src/common-structures/linked-list/linkedList.test.ts
+++ src/common-structures/linked-list/linkedList.test.ts
@@ -1,5 +1,5 @@
 import { expect, test, describe } from 'vitest';
-import { createLinkedList } from './util';
+import { createLinkedList } from './linkedList';
 
 const firstItem = 'first';
 const secondItem = 'second';
--- src/common-structures/linked-list/linkedList.ts
+++ src/common-structures/linked-list/linkedList.ts
@@ -1,7 +1,6 @@
 type LinkedList<T> = ((boolean: boolean) => T | LinkedList<T>) | null;
 
 type CreateLinkedList = <T>(entries: Array<T>) => LinkedList<T>;
-
 export const createLinkedList: CreateLinkedList = (entries) => {
   if (entries.length === 0) {
     return null;
