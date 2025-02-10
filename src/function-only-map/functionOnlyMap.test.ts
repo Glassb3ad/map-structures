129f2488777acef6aa5f1a6ddff3426150942b02
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-10 14:56:31 +0200

    Create FOMap with initial values

--- src/function-only-map/functionOnlyMap.test.ts
+++ src/function-only-map/functionOnlyMap.test.ts
@@ -1,21 +1,20 @@
 import { expect, test, describe } from 'vitest';
 import { FOMap } from './functionOnlyMap';
 
-// const firstTestPairKey = '1';
-// const firstTestPairValue = 1;
-// const firstTestPair: [string, number] = [firstTestPairKey, firstTestPairValue];
-// const testPairs: Array<[string, number]> = [firstTestPair, ['2', 2]];
+const firstTestPairKey = '1';
+const firstTestPairValue = 1;
+const firstTestPair: [string, number] = [firstTestPairKey, firstTestPairValue];
+const testPairs: Array<[string, number]> = [firstTestPair, ['2', 2]];
 
-describe('NaiveMap constructor', () => {
-  test('Create NaiveMap', () => {
+describe('FOMap constructor', () => {
+  test('Create FOMap', () => {
     const map = new FOMap();
     expect(map).toBeInstanceOf(FOMap);
   });
 
-  //   test('Create NaiveMap with initial values', () => {
-  //     const map = new NaiveMap<number>(testPairs);
-  //     testPairs.forEach((pair) => {
-  //       expect(map.map).toContainEqual(pair);
-  //     });
-  //   });
+  test('Create FOMap with initial values', () => {
+    const map = new FOMap<number>(testPairs);
+    const list = map.toList();
+    expect(list).toEqual(testPairs);
+  });
 });
--- src/function-only-map/functionOnlyMap.ts
+++ src/function-only-map/functionOnlyMap.ts
@@ -1,13 +1,24 @@
 import {
   createLinkedList,
+  fold,
   LinkedList,
 } from '../common-structures/linked-list/linkedList';
-import { Pair, toPairs } from '../common-structures/pair/pair';
+import { first, Pair, second, toPairs } from '../common-structures/pair/pair';
 
 export class FOMap<T> {
   linkedList: LinkedList<Pair<string, T>>;
 
   constructor(entries: Array<[string, T]> = []) {
     this.linkedList = createLinkedList(toPairs(entries));
+  }
+
+  toList() {
+    return fold<Pair<string, T>, Array<[string, T]>>(
+      this.linkedList,
+      (pair, acc) => {
+        return [...acc, [first(pair) as string, second(pair) as T]];
+      },
+      []
+    );
   }
 }
