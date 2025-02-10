ea400dc2525b1ed742c2733179eb6882141cf7ea
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-10 12:32:51 +0200

    FOMap class and costructor

---
+++ src/function-only-map/functionOnlyMap.ts
@@ -0,0 +1,13 @@
+import {
+  createLinkedList,
+  LinkedList,
+} from '@/common-structures/linked-list/linkedList';
+import { Pair, toPairs } from '@/common-structures/pair/pair';
+
+export class FOMap<T> {
+  linkedList: LinkedList<Pair<string, T>>;
+
+  constructor(entries: Array<[string, T]> = []) {
+    this.linkedList = createLinkedList(toPairs(entries));
+  }
+}
