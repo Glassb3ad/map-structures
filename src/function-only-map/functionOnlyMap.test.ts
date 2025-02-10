74a83a2565ba2ee0b1750e1bc5373a5102d75769
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-10 17:15:46 +0200

    refactor describe blocks

--- src/function-only-map/functionOnlyMap.test.ts
+++ src/function-only-map/functionOnlyMap.test.ts
@@ -6,80 +6,82 @@ const firstTestPairValue = 1;
 const firstTestPair: [string, number] = [firstTestPairKey, firstTestPairValue];
 const testPairs: Array<[string, number]> = [firstTestPair, ['2', 2]];
 
-describe('FOMap constructor', () => {
-  test('Create FOMap', () => {
-    const map = new FOMap();
-    expect(map).toBeInstanceOf(FOMap);
+describe('FOMap', () => {
+  describe('constructor', () => {
+    test('Create FOMap', () => {
+      const map = new FOMap();
+      expect(map).toBeInstanceOf(FOMap);
+    });
+
+    test('Create map with initial values', () => {
+      const map = new FOMap<number>(testPairs);
+      const list = map.toList();
+      expect(list).toEqual(testPairs);
+    });
   });
 
-  test('Create FOMap with initial values', () => {
-    const map = new FOMap<number>(testPairs);
-    const list = map.toList();
-    expect(list).toEqual(testPairs);
-  });
-});
+  describe('has', () => {
+    test('Check that there is pair with given key', () => {
+      const map = new FOMap<number>(testPairs);
+      expect(map.has(firstTestPairKey)).toBe(true);
+    });
 
-describe('FOMap.has', () => {
-  test('Check that there is pair with given key', () => {
-    const map = new FOMap<number>(testPairs);
-    expect(map.has(firstTestPairKey)).toBe(true);
-  });
-
-  test('Check that there is no pair with given key', () => {
-    const map = new FOMap<number>(testPairs);
-    expect(map.has('notToBeFound')).toBe(false);
-  });
-});
-
-describe('FOMap.get', () => {
-  test('get value by key', () => {
-    const map = new FOMap<number>(testPairs);
-    expect(map.get(firstTestPairKey)).toBe(firstTestPairValue);
+    test('Check that there is no pair with given key', () => {
+      const map = new FOMap<number>(testPairs);
+      expect(map.has('notToBeFound')).toBe(false);
+    });
   });
 
-  test('get undefined if there is no associated value for key', () => {
-    const map = new FOMap<number>(testPairs);
-    expect(map.get('notToBeFound')).toBeUndefined();
-  });
-});
+  describe('get', () => {
+    test('get value by key', () => {
+      const map = new FOMap<number>(testPairs);
+      expect(map.get(firstTestPairKey)).toBe(firstTestPairValue);
+    });
 
-describe('NaiveMap.set', () => {
-  test('set new key-value pair', () => {
-    const map = new FOMap<number>(testPairs);
-    map.set(['newKey', 1]);
-    expect(map.get('newKey')).toBe(1);
-  });
-
-  test('if key-value pair exists replace the old value with new', () => {
-    const map = new FOMap<number>(testPairs);
-    map.set([firstTestPairKey, 200]);
-    expect(map.get(firstTestPairKey)).toBe(200);
+    test('get undefined if there is no associated value for key', () => {
+      const map = new FOMap<number>(testPairs);
+      expect(map.get('notToBeFound')).toBeUndefined();
+    });
   });
 
-  test('Does not introduce duplicate keys when input key already exists', () => {
-    const map = new FOMap<number>(testPairs);
-    map.set([firstTestPairKey, 200]);
-    expect(
-      map.toList().filter((pair) => pair[0] === firstTestPairKey).length
-    ).toBe(1);
-  });
-});
+  describe('set', () => {
+    test('set new key-value pair', () => {
+      const map = new FOMap<number>(testPairs);
+      map.set(['newKey', 1]);
+      expect(map.get('newKey')).toBe(1);
+    });
 
-describe('NaiveMap.delete', () => {
-  test('return true when key has value', () => {
-    const map = new FOMap<number>(testPairs);
-    expect(map.delete(firstTestPairKey)).toBe(true);
+    test('if key-value pair exists replace the old value with new', () => {
+      const map = new FOMap<number>(testPairs);
+      map.set([firstTestPairKey, 200]);
+      expect(map.get(firstTestPairKey)).toBe(200);
+    });
+
+    test('Does not introduce duplicate keys when input key already exists', () => {
+      const map = new FOMap<number>(testPairs);
+      map.set([firstTestPairKey, 200]);
+      expect(
+        map.toList().filter((pair) => pair[0] === firstTestPairKey).length
+      ).toBe(1);
+    });
   });
 
-  test('remove key-value pair that matches key', () => {
-    const map = new FOMap<number>(testPairs);
-    map.delete(firstTestPairKey);
-    expect(map.toList()).not.toContainEqual(firstTestPair);
-  });
+  describe('delete', () => {
+    test('return true when key has value', () => {
+      const map = new FOMap<number>(testPairs);
+      expect(map.delete(firstTestPairKey)).toBe(true);
+    });
 
-  test('Does not remove key-value pairs that do not match key', () => {
-    const map = new FOMap<number>(testPairs);
-    map.delete(firstTestPairKey);
-    expect(map.toList()).toContainEqual(['2', 2]);
+    test('remove key-value pair that matches key', () => {
+      const map = new FOMap<number>(testPairs);
+      map.delete(firstTestPairKey);
+      expect(map.toList()).not.toContainEqual(firstTestPair);
+    });
+
+    test('Does not remove key-value pairs that do not match key', () => {
+      const map = new FOMap<number>(testPairs);
+      map.delete(firstTestPairKey);
+      expect(map.toList()).toContainEqual(['2', 2]);
+    });
   });
 });
