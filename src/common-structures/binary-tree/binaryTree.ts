775b49a79fb2f47a1c975a0917d74126e8b2434d
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-25 00:00:01 +0200

    refactor to use entries as values

--- src/common-structures/binary-tree/binaryTree.ts
+++ src/common-structures/binary-tree/binaryTree.ts
@@ -1,44 +1,51 @@
+import { Entry } from '@/map.class';
+
+const compare = (str1: string, str2: string) => {
+  const result = str1.localeCompare(str2);
+  if (result === 0) {
+    return 0;
+  }
+  return result > 0 ? 1 : -1;
+};
 export class BinaryTree<T> {
-  value: T;
+  value: Entry<T>;
   left: BinaryTree<T> | null;
   right: BinaryTree<T> | null;
-  compare: (a: T, b: T) => 1 | 0 | -1;
 
-  constructor(value: T, compare: (a: T, b: T) => 1 | 0 | -1) {
+  constructor(value: Entry<T>) {
     this.value = value;
     this.left = null;
     this.right = null;
-    this.compare = compare;
   }
 
-  get(value: T): T | null {
-    const comp = this.compare(this.value, value);
+  get(key: string): T | null {
+    const comp = compare(this.value[0], key);
     if (comp === 0) {
-      return this.value;
+      return this.value[1];
     }
     if (comp === 1 && this.left) {
-      return this.left.get(value);
+      return this.left.get(key);
     }
     if (comp === -1 && this.right) {
-      return this.right.get(value);
+      return this.right.get(key);
     }
     return null;
   }
 
-  set(value: T) {
-    const comp = this.compare(this.value, value);
+  set(value: Entry<T>) {
+    const comp = compare(this.value[0], value[0]);
     if (comp === 1) {
       if (this.left) {
         this.left.set(value);
       } else {
-        this.left = new BinaryTree<T>(value, this.compare);
+        this.left = new BinaryTree<T>(value);
       }
     }
     if (comp === -1) {
       if (this.right) {
         this.right.set(value);
       } else {
-        this.right = new BinaryTree<T>(value, this.compare);
+        this.right = new BinaryTree<T>(value);
       }
     }
     if (comp === 0) {
